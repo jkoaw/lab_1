76518e0b57edd7dd55700af0280d674592b15e71
Szymon P <jkoaw@LAPTOP_SZYMON_P>
2025-03-11 17:48:16 +0100

    Kalkulator dziala

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -43,13 +43,15 @@
             button_ode = new Button();
             button_mno = new Button();
             button_dzi = new Button();
+            button10 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(121, 264);
+            button1.Location = new Point(138, 352);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 26);
+            button1.Size = new Size(86, 35);
             button1.TabIndex = 0;
             button1.Text = "1";
             button1.UseVisualStyleBackColor = true;
@@ -57,9 +59,10 @@
             // 
             // button2
             // 
-            button2.Location = new Point(225, 264);
+            button2.Location = new Point(257, 352);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(86, 31);
             button2.TabIndex = 1;
             button2.Text = "2";
             button2.UseVisualStyleBackColor = true;
@@ -67,9 +70,10 @@
             // 
             // button3
             // 
-            button3.Location = new Point(328, 266);
+            button3.Location = new Point(375, 355);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(75, 23);
+            button3.Size = new Size(86, 31);
             button3.TabIndex = 2;
             button3.Text = "3";
             button3.UseVisualStyleBackColor = true;
@@ -77,9 +81,10 @@
             // 
             // button4
             // 
-            button4.Location = new Point(121, 209);
+            button4.Location = new Point(138, 279);
+            button4.Margin = new Padding(3, 4, 3, 4);
             button4.Name = "button4";
-            button4.Size = new Size(75, 23);
+            button4.Size = new Size(86, 31);
             button4.TabIndex = 3;
             button4.Text = "4";
             button4.UseVisualStyleBackColor = true;
@@ -87,9 +92,10 @@
             // 
             // button5
             // 
-            button5.Location = new Point(225, 209);
+            button5.Location = new Point(257, 279);
+            button5.Margin = new Padding(3, 4, 3, 4);
             button5.Name = "button5";
-            button5.Size = new Size(75, 23);
+            button5.Size = new Size(86, 31);
             button5.TabIndex = 4;
             button5.Text = "5";
             button5.UseVisualStyleBackColor = true;
@@ -97,9 +103,10 @@
             // 
             // button6
             // 
-            button6.Location = new Point(328, 209);
+            button6.Location = new Point(375, 279);
+            button6.Margin = new Padding(3, 4, 3, 4);
             button6.Name = "button6";
-            button6.Size = new Size(75, 23);
+            button6.Size = new Size(86, 31);
             button6.TabIndex = 5;
             button6.Text = "6";
             button6.UseVisualStyleBackColor = true;
@@ -107,9 +114,10 @@
             // 
             // button7
             // 
-            button7.Location = new Point(121, 154);
+            button7.Location = new Point(138, 205);
+            button7.Margin = new Padding(3, 4, 3, 4);
             button7.Name = "button7";
-            button7.Size = new Size(75, 23);
+            button7.Size = new Size(86, 31);
             button7.TabIndex = 6;
             button7.Text = "7";
             button7.UseVisualStyleBackColor = true;
@@ -117,9 +125,10 @@
             // 
             // button8
             // 
-            button8.Location = new Point(225, 154);
+            button8.Location = new Point(257, 205);
+            button8.Margin = new Padding(3, 4, 3, 4);
             button8.Name = "button8";
-            button8.Size = new Size(75, 23);
+            button8.Size = new Size(86, 31);
             button8.TabIndex = 7;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
@@ -127,9 +136,10 @@
             // 
             // button9
             // 
-            button9.Location = new Point(328, 152);
+            button9.Location = new Point(375, 203);
+            button9.Margin = new Padding(3, 4, 3, 4);
             button9.Name = "button9";
-            button9.Size = new Size(75, 23);
+            button9.Size = new Size(86, 31);
             button9.TabIndex = 8;
             button9.Text = "9";
             button9.UseVisualStyleBackColor = true;
@@ -137,9 +147,10 @@
             // 
             // button0
             // 
-            button0.Location = new Point(225, 316);
+            button0.Location = new Point(257, 421);
+            button0.Margin = new Padding(3, 4, 3, 4);
             button0.Name = "button0";
-            button0.Size = new Size(75, 23);
+            button0.Size = new Size(86, 31);
             button0.TabIndex = 9;
             button0.Text = "0";
             button0.UseVisualStyleBackColor = true;
@@ -147,16 +158,18 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(121, 97);
+            textBox1.Location = new Point(138, 129);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(282, 23);
+            textBox1.Size = new Size(322, 27);
             textBox1.TabIndex = 10;
             // 
             // button_dodo
             // 
-            button_dodo.Location = new Point(460, 149);
+            button_dodo.Location = new Point(526, 199);
+            button_dodo.Margin = new Padding(3, 4, 3, 4);
             button_dodo.Name = "button_dodo";
-            button_dodo.Size = new Size(65, 28);
+            button_dodo.Size = new Size(74, 37);
             button_dodo.TabIndex = 11;
             button_dodo.Text = "dodaj";
             button_dodo.UseVisualStyleBackColor = true;
@@ -164,9 +177,10 @@
             // 
             // button_ode
             // 
-            button_ode.Location = new Point(460, 207);
+            button_ode.Location = new Point(526, 276);
+            button_ode.Margin = new Padding(3, 4, 3, 4);
             button_ode.Name = "button_ode";
-            button_ode.Size = new Size(75, 23);
+            button_ode.Size = new Size(86, 31);
             button_ode.TabIndex = 12;
             button_ode.Text = "odejmij";
             button_ode.UseVisualStyleBackColor = true;
@@ -174,9 +188,10 @@
             // 
             // button_mno
             // 
-            button_mno.Location = new Point(460, 250);
+            button_mno.Location = new Point(526, 333);
+            button_mno.Margin = new Padding(3, 4, 3, 4);
             button_mno.Name = "button_mno";
-            button_mno.Size = new Size(75, 23);
+            button_mno.Size = new Size(86, 31);
             button_mno.TabIndex = 13;
             button_mno.Text = "pomnoz";
             button_mno.UseVisualStyleBackColor = true;
@@ -184,19 +199,31 @@
             // 
             // button_dzi
             // 
-            button_dzi.Location = new Point(460, 288);
+            button_dzi.Location = new Point(526, 384);
+            button_dzi.Margin = new Padding(3, 4, 3, 4);
             button_dzi.Name = "button_dzi";
-            button_dzi.Size = new Size(75, 23);
+            button_dzi.Size = new Size(86, 31);
             button_dzi.TabIndex = 14;
             button_dzi.Text = "dziel";
             button_dzi.UseVisualStyleBackColor = true;
             button_dzi.Click += button_dzi_Click;
             // 
+            // button10
+            // 
+            button10.Location = new Point(526, 432);
+            button10.Name = "button10";
+            button10.Size = new Size(86, 29);
+            button10.TabIndex = 15;
+            button10.Text = "rowna sie";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(933, 444);
+            ClientSize = new Size(1066, 592);
+            Controls.Add(button10);
             Controls.Add(button_dzi);
             Controls.Add(button_mno);
             Controls.Add(button_ode);
@@ -212,6 +239,7 @@
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -235,5 +263,6 @@
         private Button button_ode;
         private Button button_mno;
         private Button button_dzi;
+        private Button button10;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,7 +1,15 @@
+using System.Linq.Expressions;
+using System.Numerics;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        int pom1, pom2, wynik;
+        string tymczas = "";
+        string wyswietlacz;
+        List<char> dzialania = [];
+        List<int> liczby = [];
         public Form1()
         {
             InitializeComponent();
@@ -9,72 +17,220 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            tymczas += '1';
+            wyswietlacz += '1';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            tymczas += '2';
+            wyswietlacz += '2';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            tymczas += '3';
+            wyswietlacz += '3';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            tymczas += '4';
+            wyswietlacz += '4';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            tymczas += '5';
+            wyswietlacz += '5';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            tymczas += '6';
+            wyswietlacz += '6';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+            tymczas += '7';
+            wyswietlacz += '7';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            tymczas += '8';
+            wyswietlacz += '8';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-
+            tymczas += '9';
+            wyswietlacz += '9';
+            textBox1.Text = wyswietlacz;
         }
 
         private void button0_Click(object sender, EventArgs e)
         {
-
+            if (tymczas.Length > 0)
+            {
+                tymczas += '0';
+                wyswietlacz += '0';
+                textBox1.Text = wyswietlacz;
+            }
         }
 
         private void button_dodo_Click(object sender, EventArgs e)
         {
+            wyswietlacz += " + ";
+            for (int i = 0; i < tymczas.Length; i++)
+            {
+                if (pom1 > 0)
+                {
+                    pom1 *= 10;
+                }
+                pom1 += tymczas[i] - '0';
+            }
+            tymczas = "";
+            liczby.Add(pom1);
+            pom1 = 0;
+            dzialania.Add('1');
+            textBox1.Text = wyswietlacz;
 
         }
 
         private void button_ode_Click(object sender, EventArgs e)
         {
-
+            wyswietlacz += " - ";
+            for (int i = 0; i < tymczas.Length; i++)
+            {
+                if (pom1 > 0)
+                {
+                    pom1 *= 10;
+                }
+                pom1 += tymczas[i] - '0';
+            }
+            tymczas = "";
+            liczby.Add(pom1);
+            pom1 = 0;
+            dzialania.Add('2');
+            textBox1.Text = wyswietlacz;
         }
 
         private void button_mno_Click(object sender, EventArgs e)
         {
-
+            wyswietlacz += " * ";
+            for (int i = 0; i < tymczas.Length; i++)
+            {
+                if (pom1 > 0)
+                {
+                    pom1 *= 10;
+                }
+                pom1 += tymczas[i] - '0';
+            }
+            tymczas = "";
+            liczby.Add(pom1);
+            pom1 = 0;
+            dzialania.Add('3');
+            textBox1.Text = wyswietlacz;
         }
 
         private void button_dzi_Click(object sender, EventArgs e)
         {
+            wyswietlacz += " / ";
+            for (int i = 0; i < tymczas.Length; i++)
+            {
+                if (pom1 > 0)
+                {
+                    pom1 *= 10;
+                }
+                pom1 += tymczas[i] - '0';
+            }
+            tymczas = "";
+            liczby.Add(pom1);
+            pom1 = 0;
+            dzialania.Add('4');
+            textBox1.Text = wyswietlacz;
+        }
 
+        private void button10_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < tymczas.Length; i++)
+            {
+                if (pom1 > 0)
+                {
+                    pom1 *= 10;
+                }
+                pom1 += tymczas[i] - '0';
+                
+            }
+            tymczas = "";
+            liczby.Add(pom1);
+            pom1 = 0;
+            for (int i = 0; i < dzialania.Count; i++)
+            {
+                if (dzialania[i] == '3' || dzialania[i] == '4')
+                {
+                    switch (dzialania[i])
+                    {
+                        case '3':
+                            liczby[i] *= liczby[i + 1];
+                            break;
+                        case '4':
+                            liczby[i] /= liczby[i + 1];
+                            break;
+                        default:
+                            break;
+                    }
+                    liczby.RemoveAt(i + 1);
+                    dzialania.RemoveAt(i);
+                    i--;
+                }
+            }
+            wynik += liczby[0];
+            liczby.RemoveAt(0);
+            while (dzialania.Count > 0)
+            {
+                switch (dzialania[0])
+                {
+                    case '1':
+                        wynik += liczby[0];
+                        liczby.RemoveAt(0);
+                        dzialania.RemoveAt(0);
+                        break;
+                    case '2':
+                        wynik -= liczby[0];
+                        liczby.RemoveAt(0);
+                        dzialania.RemoveAt(0);
+                        break;
+                    case '3':
+                        wynik *= liczby[0];
+                        liczby.RemoveAt(0);
+                        dzialania.RemoveAt(0);
+                        break;
+                    case '4':
+                        wynik /= liczby[0];
+                        liczby.RemoveAt(0);
+                        dzialania.RemoveAt(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            wyswietlacz = Convert.ToString(wynik, 10);
+            textBox1.Text = wyswietlacz;
+            wynik = 0;
+            liczby.Clear();
+            dzialania.Clear();
+            wyswietlacz = "";
         }
     }
 }
